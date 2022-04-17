6a0d9fa380e5fd80b6a92be9d8c2d5f37aba1821
NyakTwomanAnshari <oman.anshari@gmail.com>
2022-04-17 07:18:29 +0000

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://api.oman.studentdumbways.my.id/api/v1"
+    baseURL: "https://api.oman.studentdumbways.my.id/api/v1"
 });
 
 const setAuthToken = (token) => {
